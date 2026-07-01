1bc4d6c0607f37cf2859e4bfab32ee156799cffc
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-07-01 23:40:55 +0300

    feat: add http server

---
+++ internal/server/http/server.go
@@ -0,0 +1,38 @@
+package httpserver
+
+import (
+	"chat-gateway/internal/config"
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// Wrapper for http server
+type Server struct {
+	httpServer *http.Server
+}
+
+// Create new http server
+func NewServer(config *config.ServerConfig, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:              fmt.Sprintf("%s:%s", config.Host, config.Port),
+			MaxHeaderBytes:    config.MaxHeaderBytes,
+			Handler:           handler,
+			ReadTimeout:       config.ReadTimeout,
+			WriteTimeout:      config.WriteTimeout,
+			ReadHeaderTimeout: config.ReadHeaderTimeout,
+			IdleTimeout:       config.IdleTimeout,
+		},
+	}
+}
+
+// Launch http server
+func (s *Server) Start() error {
+	return s.httpServer.ListenAndServe()
+}
+
+// Shutdown http server with context
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
