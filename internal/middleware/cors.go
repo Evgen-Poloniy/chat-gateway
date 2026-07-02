e944248d5632b9b6e916ae915e2f778c8f98e307
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-07-02 17:50:22 +0300

    feat: add CORS politics

---
+++ internal/middleware/cors.go
@@ -0,0 +1,14 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
+
+		c.Next()
+	}
+}
