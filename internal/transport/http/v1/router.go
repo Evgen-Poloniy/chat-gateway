9279408c9260a75986df25df198e1e83ef88c941
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-07-05 18:36:10 +0300

    feat: add endpoint for creating direct chat

--- internal/transport/http/v1/router.go
+++ internal/transport/http/v1/router.go
@@ -8,5 +8,6 @@ func NewRouter(router *gin.Engine, handler *Handler) {
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("/register", handler.RegisterUser)
+		v1.POST("/create/chat/direct", handler.CreateDirectChat)
 	}
 }
