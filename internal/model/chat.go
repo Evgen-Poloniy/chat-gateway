c1f5d033423411d53d565fa4bf2e609632a5fb97
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-07-10 18:51:02 +0300

    feat: add data models for database and message broker

---
+++ internal/model/chat.go
@@ -0,0 +1,39 @@
+package model
+
+import "time"
+
+// DirectChat represents database model for creating direct chat.
+type DirectChat struct {
+	ChatID         int64
+	ParticipantIDs []int64
+	CreatedAt      time.Time
+}
+
+// GroupChat represents database model for creating direct chat.
+type GroupChat struct {
+	ChatID         int64
+	ParticipantIDs []int64
+	Name           string
+	Title          *string
+	Description    *string
+	CreatedAt      time.Time
+	OwnerID        int64
+}
+
+// Chat represents model of chat
+type Chat struct {
+	ChatID      int64     `db:"id"`
+	ChatType    string    `db:"type"`
+	Name        *string   `db:"name"`
+	Title       *string   `db:"title"`
+	Description *string   `db:"description"`
+	CreatedAt   time.Time `db:"created_at"`
+	OwnerID     *int64    `db:"owner_id"`
+}
+
+// SendMessage represents model for message broker
+type SendMessage struct {
+	ChatID   int64  `json:"chat_id"`
+	SenderID int64  `json:"sender_id"`
+	Message  string `json:"message" binding:"required"`
+}
---
+++ internal/model/user.go
@@ -0,0 +1,27 @@
+package model
+
+import "time"
+
+// User represents user data into messenger.
+type User struct {
+	UserID    int64      `db:"id"`
+	Username  string     `db:"username"`
+	Email     *string    `db:"email"`
+	FirstName *string    `db:"first_name"`
+	LastName  *string    `db:"last_name"`
+	BirthDate *time.Time `db:"birth_date"`
+	CreatedAt time.Time  `db:"created_at"`
+	Gender    *string    `db:"gender"`
+}
+
+// UpdateUser represents user updating data into messenger.
+type UpdateUser struct {
+	UserID    int64      `db:"id"`
+	Username  *string    `db:"username"`
+	Email     *string    `db:"email"`
+	FirstName *string    `db:"first_name"`
+	LastName  *string    `db:"last_name"`
+	BirthDate *time.Time `db:"birth_date"`
+	CreatedAt time.Time  `db:"created_at"`
+	Gender    *string    `db:"gender"`
+}
